630cab80a6d1c9bcaa557db7f66f58595c61e704
CodingSinceThe80s <razvan.hrestic@unibw.de>
2022-04-01 16:55:16 +0200

    Testing cluster features 1.4.22

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (request, response) => {
+    response.statusCode = 200;
+    response.send("Hello world");
+})
+
+app.listen(port, () => {})
